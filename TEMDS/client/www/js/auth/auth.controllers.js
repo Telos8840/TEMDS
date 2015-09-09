7fb7fb7d210dd7cde3f3b018470f82fdc4ed6f87
Geon Lee <keon2567@gmail.com>
2015-09-09 00:17:55 -0700

    New Env Setup. What a waste of time.

--- TEMDS/client/www/js/auth/auth.controllers.js
+++ TEMDS/client/www/js/auth/auth.controllers.js
@@ -1,22 +1,22 @@
 angular.module('temds.auth.controllers', [])
 
 
-.controller('WelcomeCtrl', function($scope, $state, $ionicModal) {
+.controller('WelcomeCtrl', function ($scope, $state, $ionicModal) {
     // $scope.bgs = ["http://lorempixel.com/640/1136"];
     $scope.bgs = ["img/welcome-bg.jpeg"];
 
     /// Sign In button is pressed
     /// Goto Sign In View
-    $scope.signInForm = function() {
-      console.log("display sign-in form");
-      $state.go('sign-in');
+    $scope.signInForm = function () {
+        console.log("display sign-in form");
+        $state.go('sign-in');
     }
 
-    $scope.signUp = function() {
-      /// <summary>
-      /// Open new user registration step 1: Email Confirmation Form
-      /// </summary>
-      $state.go('email-confirm');
+    $scope.signUp = function () {
+        /// <summary>
+        /// Open new user registration step 1: Email Confirmation Form
+        /// </summary>
+        $state.go('email-confirm');
     }
 
     /* keeping for reference use
@@ -26,102 +26,103 @@ angular.module('temds.auth.controllers', [])
     }; */
 
     $ionicModal.fromTemplateUrl('views/app/legal/privacy-policy.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
+        scope: $scope,
+        animation: 'slide-in-up'
     }).then(function (modal) {
-      $scope.privacy_policy_modal = modal;
+        $scope.privacy_policy_modal = modal;
     });
 
     $ionicModal.fromTemplateUrl('views/app/legal/terms-of-service.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
+        scope: $scope,
+        animation: 'slide-in-up'
     }).then(function (modal) {
-      $scope.terms_of_service_modal = modal;
+        $scope.terms_of_service_modal = modal;
     });
 
     $scope.showPrivacyPolicy = function () {
-      $scope.privacy_policy_modal.show();
+        $scope.privacy_policy_modal.show();
     };
 
     $scope.showTerms = function () {
-      $scope.terms_of_service_modal.show();
+        $scope.terms_of_service_modal.show();
     };
-  })
+})
 
 /* Register Account Controller */
 .controller('RegisterAccountCtrl', function ($scope, RegisterService, $state, $ionicPopup, $interval) {
-  $scope.user = {};
-  $scope.sendConfirmText = "Send Confirmation Email";
-  $scope.timerInvalid = false;
+    $scope.user = {};
+    $scope.sendConfirmText = "Send Confirmation Email";
+    $scope.timerInvalid = false;
 
-  var sendConfirmTimer, sendConfirmTime = 0;
+    var sendConfirmTimer, sendConfirmTime = 0;
 
-  $scope.sendRegisterEmailConfirmation = function () {
-    RegisterService.sendRegisterEmailConfirmation($scope.user.email)
-    .then(function(data) {
-      if (data.startsWith('success')) {
+    $scope.sendEmailConfirmation = function () {
+        RegisterService.sendRegisterEmailConfirmation($scope.user.email)
+            .then(function (data) {
+                if (data.startsWith('success')) {
+                    $ionicPopup.alert({
+                        title: 'Confirmaion Sent',
+                        content: 'Email has been sent to ' + $scope.user.email 
+                                + ' with five digit confirmation numbers.'
+                    }).then(function () {
+                        // set timer to limit spam calls
+                        sendConfirmTime = 120;
+                        $scope.timerInvalid = true;
+                        $scope.sendConfirmText = "Please wait " + sendConfirmTime + " seconds..."
+
+                        sendConfirmTimer = $interval(function () {
+                            if (sendConfirmTime <= 1) {
+                                $interval.cancel(sendConfirmTimer);
+                                sendConfirmTimer = undefined;
+                                $scope.sendConfirmText = "Send Confirmation Email";
+                                $scope.timerInvalid = false;
+                            } else {
+                                $scope.sendConfirmText = "Please wait " + (--sendConfirmTime)
+                                                        + " seconds..."
+                            }
+                        }, 1000);
+                    });
+                } else {
+                    $ionicPopup.alert({
+                        title: 'Error',
+                        content: data
+                    });
+                }
+            });
+    };
+
+    $scope.checkConfirmation = function () {
+        //TODO: CHECK CONFIRMATION NUMBER
         $ionicPopup.alert({
-          title: 'Confirmaion Sent',
-          content: 'Email has been sent to '+$scope.user.email 
-                  +' with five digit confirmation numbers.'
-        }).then(function() {
-          // set timer to limit spam calls
-          sendConfirmTime = 10; // TODO: Set 120 sec instead. 10 sec is too short....
-          $scope.timerInvalid = true;
-          $scope.sendConfirmText = "Please wait "+sendConfirmTime+" seconds..."
-
-          sendConfirmTimer = $interval(function() {
-            if (sendConfirmTime <= 1) {
-              $interval.cancel(sendConfirmTimer);
-              sendConfirmTimer = undefined;
-              $scope.sendConfirmText = "Send Confirmation Email";
-              $scope.timerInvalid = false;
-            } else {
-              $scope.sendConfirmText = "Please wait "+(--sendConfirmTime)+" seconds..."
-            }
-          }, 1000);
+            title: 'Email Confirmed',
+            content: $scope.user.confirmNum + ' matched with email, ' // temp msg....
+                + $scope.user.email + "!"
+        }).then(function () {
+            $state.go('register-form');
         });
-      } else {
-        $ionicPopup.alert({
-          title: 'Error',
-          content: data
-        });
-      }
-    });
-  };
-
-  $scope.checkConfirmation = function () {
-    //TODO: CHECK CONFIRMATION NUMBER
-    $ionicPopup.alert({
-      title: 'Email Confirmed',
-      content: $scope.user.confirmNum + ' matched with email, ' // temp msg....
-              + $scope.user.email+"!"
-    }).then(function() {
-      $state.go('register-form');
-    });
-  };  
+    };
 })
 
 .controller('WelcomeBackCtrl', function ($scope, $state, $ionicModal) {
-  $scope.doLogIn = function () {
-    console.log("doing log in");
-    $state.go('app.feed');
-  };
+    $scope.doLogIn = function () {
+        console.log("doing log in");
+        $state.go('app.feed');
+    };
 
-  $ionicModal.fromTemplateUrl('views/auth/forgot-password.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function (modal) {
-    $scope.forgot_password_modal = modal;
-  });
+    $ionicModal.fromTemplateUrl('views/auth/forgot-password.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.forgot_password_modal = modal;
+    });
 
-  $scope.showForgotPassword = function () {
-    $scope.forgot_password_modal.show();
-  };
+    $scope.showForgotPassword = function () {
+        $scope.forgot_password_modal.show();
+    };
 
-  $scope.requestNewPassword = function () {
-    console.log("requesting new password");
-  };
+    $scope.requestNewPassword = function () {
+        console.log("requesting new password");
+    };
 
     // //Cleanup the modal when we're done with it!
     // $scope.$on('$destroy', function() {
--- TEMDS/client/www/js/auth/auth.services.js
+++ TEMDS/client/www/js/auth/auth.services.js
@@ -1,20 +1,46 @@
 angular.module('temds.auth.services', [])
 
-.service('RegisterService', function ($http, $q){
-  // Signup Step #1: Send confirmation email
-  this.sendRegisterEmailConfirmation = function(email) {
-    if (email && email.trim() != '') {
-      var dfd = $q.defer();
+.service('RegisterService', function ($http, $q) {
+    // Signup Step #1: Send confirmation email
+    this.sendEmailConfirmation = function (email) {
+        if (email && email.trim() != '') {
+            var dfd = $q.defer();
 
-      $http.post(_API_HOST_ + 'api/user/register', {email: email})
-      .success(function(response) {
-        dfd.resolve('success: ' + response.data);
-      }).catch(function(response) {
-        dfd.resolve('error: ' + response.data.message);
-      });
+            $http.post(_API_HOST_ + 'api/user/emailConfirmation', {
+                    email: email
+                }).success(function (response) {
+                    dfd.resolve('success: ' + response.data);
+                }).catch(function (response) {
+                    dfd.resolve('error: ' + response.data.message);
+                });
 
-      return dfd.promise;
+            return dfd.promise;
+        }
+        return 'error: empty email'
+    };
+    
+    /**
+     * Check if confirmation number matches with the email address.
+     * @param   {String} email      User email address
+     * @param   {String} confirmNum Five digit number
+     * @returns {Boolean} true / false
+     */
+    this.checkEmailConfirmation = function (email, confirmNum) {
+        
+        if (email && confirmNum) {
+            var dfd = $q.defer();
+
+            $http.get(_API_HOST_ + 'api/user/emailConfirmation', {
+                    email: email,
+                    confirmNum: confirmNum
+                }).success(function (response) {
+                    dfd.resolve(true);
+                }).catch(function (response) {
+                    dfd.resolve(false);
+                });
+
+            return dfd.promise;
+        }
+        return false;
     }
-    return 'error: empty email'
-  };
 });
