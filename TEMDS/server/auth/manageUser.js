361ed44ceaada8202662b385a19e8d349bbb2193
Saul Guardado <seguardado88@gmail.com>
2015-09-14 01:08:03 -0700

    Created new address API

--- TEMDS/server/auth/manageUser.js
+++ TEMDS/server/auth/manageUser.js
@@ -5,9 +5,10 @@
  * Time: 5:37 PM
  */
 
-var response = require('../helpers/response');
-var pwdMgr = require('./managePasswords');
-var nodeMailer = require('nodemailer');				//http://blog.nodeknockout.com/post/34641712180/sending-email-from-nodejs
+var response    = require('../helpers/response');
+var pwdMgr      = require('./managePasswords');
+var nodeMailer  = require('nodemailer');				//http://blog.nodeknockout.com/post/34641712180/sending-email-from-nodejs
+var _           = require('lodash');
 
 //TODO: Get SMTP authentication from businessowner
 var smtpTransport = nodeMailer.createTransport("SMTP",{
@@ -105,7 +106,7 @@ module.exports = function (server, db) {
             var cnfrm = (""+Math.random()).substring(2,7);
             penUser.ConfirmationNumber = cnfrm;
 
-            db.pending_users.update({Email: userEmail}, { $set: {ConfirmationNumber: cnfrm, ModifiedDate: new Date()} },
+            db.pending_users.update({_id: db.ObjectId(penUser._id)}, { $set: {ConfirmationNumber: cnfrm, ModifiedDate: new Date()} },
               function (err, data) {
                 if(err) response.error(res, "Error updating activation number for " + userEmail, err);
               });
@@ -164,7 +165,7 @@ module.exports = function (server, db) {
 
         if(confirmNum == dbConfirm) {
 
-          db.pending_users.update({Email: email}, { $set: {Activated: true, ModifiedDate: new Date()} },
+          db.pending_users.update({_id: db.ObjectId(dbUser._id)}, { $set: {Activated: true, ModifiedDate: new Date()} },
             function (err, data) {
               if(err) response.error(res, "Error updating user " + email, err);
               else response.success(res, "User activated " + email);
@@ -231,7 +232,7 @@ module.exports = function (server, db) {
                 LastName: user.lName,
                 Phone: user.phoneNum,
                 Birthday: user.bday,
-                Address: {},
+                Address: [],
                 InsertDate: new Date(),
                 ModifiedDate: new Date()
               };
@@ -248,6 +249,113 @@ module.exports = function (server, db) {
           response.error(res, "User hasn't used confirmation number yet - " + user.email, err);
         }
       });
+    });
+    return next();
+  });
+
+  /**
+   * Adds new address to given users list of existing addresses
+   *
+   * @param id
+   * @param name
+   * @param addr1
+   * @param addr2
+   * @param city
+   * @param state
+   * @param zipcode
+   * @param primary
+   */
+  server.post('api/user/addAddress', function (req, res, next) {
+    console.log("\n *** Adding new address *** \n");
+
+    var detail = req.params;
+
+    db.user_detail.findOne({UserKey: db.ObjectId(detail.id)}, function (err, dbDetail) {
+      if (err) response.error(res, "Error finding user_detail id", err);
+      else if (!dbDetail) response.error(res, "Can't find id in user detail " + detail.id, err);
+      else {
+
+        var newAdd = {
+            Name: detail.name,
+            Addr1: detail.addr1,
+            Addr2: detail.addr2,
+            City: detail.city,
+            State: detail.state,
+            Zipcod: detail.zipcode,
+            Primary: detail.primary
+          };
+
+        /**dbDetail.Address = [
+            {
+              "id": "000000000002",
+              "name": "Work",
+              "addr1": "666 Devil Blvd",
+              "addr2": "Ste #6",
+              "city": "Los Angeles",
+              "state": "CA",
+              "zipcode": 90006,
+              "primary": false
+            },
+            {
+              "id": "000000000003",
+              "name": "Bryant's Office",
+              "addr1": "4321 Wilshire Blvd.",
+              "addr2": "Ste 1320",
+              "city": "Los Angeles",
+              "state": "CA",
+              "zipcode": 90004,
+              "primary": false
+            },
+            {
+              "id": "000000000004",
+              "name": "Jade's grandmother's apartment",
+              "addr1": "2610 West 3rd Street",
+              "addr2": "",
+              "city": "Los Angeles",
+              "state": "CA",
+              "zipcode": 90057,
+              "primary": false
+            },
+            {
+              "id": "000000000005",
+              "name": "Mom",
+              "addr1": "931 Andover Dr",
+              "addr2": "Fl. 3",
+              "city": "Burbank",
+              "state": "CA",
+              "zipcode": 91504,
+              "primary": false
+            },
+            {
+              "id": "000000000006",
+              "name": "Uncle Bob",
+              "addr1": "13100 Prairie Ave",
+              "addr2": "",
+              "city": "Hawthorne",
+              "state": "CA",
+              "zipcode": 90250,
+              "primary": false
+            }
+
+          ];**/
+
+        if(detail.primary == true) {
+          _.forEach(dbDetail.Address, function (n) {
+            if(n.Primary) n.Primary = false;
+          });
+
+          dbDetail.Address.unshift(newAdd);
+        } else {
+          dbDetail.Address.push(newAdd);
+        }
+
+        db.user_detail.update({UserKey: db.ObjectId(detail.id)},
+          {$set: {Address: dbDetail.Address, ModifiedDate: new Date()}}, function (err, data) {
+          if (err) response.error(res, "Error updating details for " + detail.id, err);
+        });
+
+        response.success(res);
+      }
     });
     return next();
   });
