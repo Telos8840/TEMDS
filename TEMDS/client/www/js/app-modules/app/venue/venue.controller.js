1b296c7f850082e85226575e99c7d0424fb58008
Geon Lee <keon2567@gmail.com>
2015-10-06 23:38:09 -0700

    36: Default Sort

--- TEMDS/client/www/js/app-modules/app/venue/venue.controller.js
+++ TEMDS/client/www/js/app-modules/app/venue/venue.controller.js
@@ -1,14 +1,15 @@
 angular.module('temds.app.controllers')
 
 
-.controller('VenueListCtrl', function ($scope, $state, VenueService) {
-    $scope.venueList = [];
+.controller('VenueListCtrl', function ($scope, $state, $filter, VenueService) {
+    $scope.sortedVenueList = {}; // this should be displayed
+    $scope.venueList = []; // this is the raw list data from server
 
 
     $scope.refreshData = function () {
         VenueService.loadVenueList()
             .then(function (data) {
-                $scope.venueList = data.list;
+                $scope.venueList = $filter('orderBy')(data.list, '+name', false);
                 $scope.$broadcast('scroll.refreshComplete');
             });
     };
@@ -17,9 +18,23 @@ angular.module('temds.app.controllers')
     $scope.getList = function () {
         VenueService.loadVenueList()
             .then(function (data) {
-                $scope.venueList = data.list;
+                $scope.venueList = $filter('orderBy')(data.list, '+name', false);
                 $scope.$broadcast('scroll.infiniteScrollComplete');
             });
     };
     $scope.getList();
+
+
+    $scope.sortByName = function () {
+        var l = {};
+
+        for (var i in $scope.venueList) {
+            var letter = $scope.venueList[i].name.charAt(0).toUpperCase();
+            if (!letter.match(/[a-zA-Z]/)) letter = "#";
+            if (!l[letter]) l[letter] = [];
+            l[letter].push($scope.venueList[i]);
+        }
+
+        $scope.sortedVenueList = l;
+    };
 });
