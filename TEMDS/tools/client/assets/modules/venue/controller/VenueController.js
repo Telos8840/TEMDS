a53952e6ed750a6cdb7a9ba06901caeb163e7b7c
Saul Guardado <seguardado88@gmail.com>
2015-10-19 22:33:00 -0700

    Added more bindings

--- TEMDS/tools/client/assets/modules/venue/controller/VenueController.js
+++ TEMDS/tools/client/assets/modules/venue/controller/VenueController.js
@@ -8,12 +8,17 @@
 angular.module('venue')
   .controller('VenueController', function ($scope, VenueFactory) {
     $scope.venue = {
-      address: {},
-      hours: {}
+      address: {}
+      //hours: {}
     };
     $scope.times = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
     $scope.days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+    $scope.categories = ["Accessories", "Alcohol", "Appliances", "Arts / Crafts", "Automotive", "Beauty", "Books", "Clothing", "Electronics", "Garden", "Grocery", "Health", "Hardware", "Jewelry", "Office", "Other", "Personal Products", "Pet", "Sports", "Toys"];
+    $scope.tags = ["breakfast", "lunch", "dinner"];
     $scope.addVenue = function (venue) {
+      console.log("called", venue);
+
       VenueFactory.addVenue(venue);
     };
   });
+
