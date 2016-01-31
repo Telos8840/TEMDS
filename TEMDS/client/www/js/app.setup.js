2e1d659f82c3e506f2e5183f38357da9697df56f
Geon Lee <keon2567@gmail.com>
2016-01-31 14:30:00 -0800

    BUG: Scroll bug, header crop bug, map bug, Keyboard bug

--- TEMDS/client/www/js/app.setup.js
+++ TEMDS/client/www/js/app.setup.js
@@ -95,7 +95,8 @@ angular.module('temds', [
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
             }
             if (window.StatusBar) {
                 StatusBar.styleDefault();
